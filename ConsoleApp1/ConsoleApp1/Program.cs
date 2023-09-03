ef413fde498cee65d31e1219bdb44848a579d095
Bona Chiang <mingtsun17@gmail.com>
2023-09-03 11:40:13 +0800

    feat: (11.0)(RequiredMembers)perperties設定可以少一點code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,52 +1,89 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Hello, World!");
+
+// C# 1.0 Properties
+var fred = new learncsharp.Properties1_0("Fred", 1);
+Console.WriteLine(fred.ToString());
+
+// C# 3.0 default constructor with initialize list Properties
+var fred3 = new learncsharp.DefaultConstructorProperties3_0
+{
+    Name = "Fred",
+    Age = 3
+};
+Console.WriteLine(fred3.ToString());
+
+// C# 9.0 init Properties
+var fred9 = new learncsharp.InitProperties9_0
+{
+    Name = "Fred",
+    Age = 9
+    // Age = 9
+};
+Console.WriteLine(fred9.ToString());
+
+// C# 11.0 required Properties
+var fred11 = new learncsharp.RequiredProperties11_0
+{
+    Name = "Fred",
+    Age = 11
+};
+// fred11.Age = 10;
+Console.WriteLine(fred11.ToString());
+
 namespace learncsharp
 {
-    class ConsoleApp1
+    // C# 1.0 Properties
+    class Properties1_0
     {
-        static void Main(string[] args)
+        public string Name;
+        public int Age;
+        public Properties1_0(string name, int age)
         {
-            Console.WriteLine("Hello, World!");
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return $"{Name} ({Age})";
+        }
+    }
 
-            // I
-            //  "love"
-            //  you!
+    // C# 3.0 default constructor with properties
+    class DefaultConstructorProperties3_0
+    {
+        // 3.1 因為沒有 constructor, 要給 nullable(Warning)
+        public string? Name;
+        // 3.2 希望增加封閉性, getter、setter, private set
+        public int Age { get; /*private*/ set; }
+        public override string ToString()
+        {
+            return $"{Name} ({Age})";
+        }
+    }
 
-            // C# 1.0 Regular string literals
-            string regular1_0 = "I\n \"love\"\n\tyou!";
-            Console.WriteLine(regular1_0);
-            // C# 1.0 Verbatim string literals
-            // 你可以直接在字串中加入「斷行符號」且不需要用 \r\n 跳脫字元，但雙引號要改用 "" 才行！
-            string verbatim1_0 = @"I
- ""love""
-	you!";
-            Console.WriteLine("{0}", verbatim1_0);
+    // C# 9.0 init constructor with properties
+    class InitProperties9_0
+    {
+        public string? Name;
+        // 9.1 因為可以不給, 會跑預設值 Age = 0
+        public int Age { get; init; }
+        public override string ToString()
+        {
+            return $"{Name} ({Age})";
+        }
+    }
 
-            // C# 6.0 Interpolated Strings
-            // 透過 $"..." 可以做到「字串內插」的表達方式，字串中的 {name} 可以用來插入其他變數！
-            string interpolated6_0 = $"I\n \"love\"\n\tyou!";
-            Console.WriteLine($"{interpolated6_0}"); // {interpolated6_0} < 這個用法
-
-            // C# 8.0 Interpolated Verbatim Strings
-            // 你可以使用 @$"..." 或 $@"..." 語法，混合 Verbatim string literals 與 Interpolated Strings 的表達方式！
-            string interpolatedVerbatim8_0 = $@"I
- ""love""
-	you!";
-            Console.WriteLine(@$"{interpolatedVerbatim8_0}"); // $@ == @$
-
-            // C# 10.0 Constant interpolated strings
-            // 你可以將字串設定為 const (常數) 時，也同時使用 $ 字串內插的表達方式！
-            const string constInterpolated6_0 = "I\n \"love\"\n\tyou!";
-            Console.WriteLine($"{constInterpolated6_0}");
-
-            // C# 11.0 Row string literal
-            // 終於可以在 C# 寫雙引號不用寫成 \" 或 "" 的樣子了，這樣蠻好的！
-            string row6_0 = """
-                I
-                 "love"
-                	you!
-                """;
-            Console.WriteLine($"{row6_0}");
+    // C# 11.0 init constructor with properties
+    class RequiredProperties11_0
+    {
+        // 還可以順便拿掉 ?
+        public required string Name { get; init; }
+        public required int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} ({Age})";
         }
     }
 }  // namespace learncsharp
